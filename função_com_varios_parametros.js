149598f328df45e4621438f6fd2a63ba510bf594
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-06-19 17:23:21 -0300

    mais sobre funções com vários parâmetros

--- função_com_varios_parametros.js
+++ função_com_varios_parametros.js
@@ -18,8 +18,25 @@ console.log(subtrair(5, 2));
 function multiplicacao(a, b){
     return a * b;
 }
-console.log(multiplicacao(2,3));
-console.log(multiplicacao(4, 12));
+//console.log(multiplicacao(2,3));
+//console.log(multiplicacao(4, 12));
+console.log("----------");
+
+function criarMultiplicador(fator){
+    return function(numero){
+        return numero * fator;
+    };
+}
+const dobrar = criarMultiplicador(2);
+const triplicar = criarMultiplicador(3);
+const quintriplicar = criarMultiplicador(5);
+
+console.log(dobrar(5));
+console.log(triplicar(5));
+console.log(quintriplicar(5));
+
+console.log("----------");
+
 
 //Função de dividir
 
