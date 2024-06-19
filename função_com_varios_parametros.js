dfcb1cec87411d6d29080fa24d7bc61a01360180
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-06-19 17:02:03 -0300

    funções com vários parâmetros

---
+++ função_com_varios_parametros.js
@@ -0,0 +1,31 @@
+//Função de somar
+
+function soma(a, b){
+    return a + b;
+}
+console.log(soma(2, 2));
+
+//função de subtrair
+
+function subtrair(a, b){
+    return a - b;
+}
+console.log(subtrair(2, 5)); //leva em consideração o maior número sendo negativo o resultado
+console.log(subtrair(5, 2));
+
+//Função de multiplicar
+
+function multiplicacao(a, b){
+    return a * b;
+}
+console.log(multiplicacao(2,3));
+console.log(multiplicacao(4, 12));
+
+//Função de dividir
+
+function dividir(a, b){
+    return a / b;
+}
+console.log(dividir(3, 5));
+console.log(dividir(4, 2));
+console.log(dividir(10, 100));
