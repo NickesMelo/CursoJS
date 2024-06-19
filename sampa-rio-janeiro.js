3f6b6207b7d77a16c8e053cfde718ff393b8645c
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-06-19 12:33:12 -0300

    sampa-rio-janeiro - split(), reverse(), join()

---
+++ sampa-rio-janeiro.js
@@ -0,0 +1,7 @@
+//Sampa-Rio-Janeiro => Separar, Reverter, Juntar => split(), reverse(), join()
+
+let stringEntrada = 'Olá, mundo!';
+let stringInvertida = stringEntrada.split("").reverse().join("");
+
+console.log(stringInvertida);
+
