a9811990dda3d97dace3b0560b508d15e859329c
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-06-27 10:06:47 -0300

    Areatext desabilitada e add função edit com foco no input

--- To-do-list/assets/scripts/scripts.js
+++ To-do-list/assets/scripts/scripts.js
@@ -1,41 +1,59 @@
 const container_general = document.querySelector("#container-general");
+const priority_group = document.querySelector("#priority-group");
 const btn_add_new_note = document.querySelector("#btn-add-new-note");
 const ul = document.querySelector("ul");
-const priority_group = document.querySelector("#priority-group");
-const save = document.querySelector();
+const li = document.createElement("li");
+const multilineInput = document.querySelector("#multilineInput");
+const btn_save = document.querySelector(".btn-save");
+const btn_edit = document.querySelector(".btn-edit");
+const btn_delete = document.querySelector(".btn-delete");
+const btn_return = document.querySelector(".btn-return");
 
-function prevent_default(){
-    prevent_default();
-}
+multilineInput.disabled = true;
 
-function create_new_note(event){
+function prevent_default(event) {
     event.preventDefault();
-    create_new_li();
-    console.log("btn add clicado");
-    
 }
 
-function create_new_li(){
-    const li = document.createElement("li");
-    li.innerHTML = `
+function createNewNote(event){
+    event.preventDefault(event);
+    createNewLi();
+    editNote();
+}
+
+function createNewLi(){
+    
+    li.classList.add("low-priority");
+    li.innerHTML = 
+    `
         <li>
-                <div class="notes">
-                    <input type="checkbox" name="checkbox-to-do" id="checkbox-to-do">
-                    <textarea id="multilineInput" name="multilineInput" rows="4" cols="50" maxlength="80" placeholder="Escreva sua nota"></textarea>
-                </div>
-                <div class="group-buttons">
-                    <button class="btn-save">Salvar</button>
-                    <button class="btn-edit">Editar</button>
-                    <button class="btn-delete">Excluir</button>
-                    <button class="btn-return">Voltar</button>
-                </div>
-            </li>
+            <div class="notes">
+                <input type="checkbox" name="checkbox-to-do" id="checkbox-to-do">
+                <textarea id="multilineInput" name="multilineInput" rows="4" cols="50" maxlength="80" placeholder="Escreva sua nota"></textarea>
+            </div>
+            <div class="group-buttons">
+                <button class="btn-save">Salvar</button>
+                <button class="btn-edit">Editar</button>
+                <button class="btn-delete">Excluir</button>
+                <button class="btn-return">Voltar</button>
+            </div>
+        </li>
     `
     ul.appendChild(li);
 }
 
-function select_priority_group(){
+function selectPriorityGroup(){
+    console.log(priority-group);
+}
+
+function editNote(){
+    multilineInput.disabled = false;
+    multilineInput.focus();
+    multilineInput.select();
+
+    console.log(btn_edit);
 
 }
 
-btn_add_new_note.addEventListener("click", create_new_note);
+btn_add_new_note.addEventListener("click", createNewNote);
+btn_edit.addEventListener("click", editNote);
