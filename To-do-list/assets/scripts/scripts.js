4980faf34c6a81a9de30632be560915a91db6afd
Nicolas Melo <141958895+NickesMelo@users.noreply.github.com>
2024-06-27 11:16:12 -0300

    correções na fução createNewLi()

--- To-do-list/assets/scripts/scripts.js
+++ To-do-list/assets/scripts/scripts.js
@@ -15,20 +15,18 @@ function createNewNote(event){
 function createNewLi(){
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
-    li.innerHTML = `    <li">
-                            <div class="notes">
-                                <input type="checkbox" name="checkbox-to-do" id="checkbox-to-do">
-                                <textarea id="multilineInput" name="multilineInput" rows="4" cols="50" maxlength="80" placeholder="Escreva sua nota"></textarea>
-                            </div>
-                            <div class="group-buttons">
-                                <button class="btn-save">Salvar</button>
-                                <button class="btn-edit">Editar</button>
-                                <button class="btn-delete">Excluir</button>
-                                <button class="btn-return">Voltar</button>
-                            </div>
-                        </li>
-                        `
-                ul.append(li)
+    li.innerHTML = `    <div class="notes">
+                            <input type="checkbox" name="checkbox-to-do" id="checkbox-to-do">
+                            <textarea id="multilineInput" name="multilineInput" rows="4" cols="50" maxlength="80" placeholder="Escreva sua nota"></textarea>
+                        </div>
+                        <div class="group-buttons">
+                            <button class="btn-save">Salvar</button>
+                            <button class="btn-edit">Editar</button>
+                            <button class="btn-delete">Excluir</button>
+                            <button class="btn-return">Voltar</button>
+                        </div>
+                    `
+                ul.append(li);
                 return li;
 }
 
